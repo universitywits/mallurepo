311dc7dcec4b80eaedc6f33563148f56437235b5
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2021-06-14 10:21:34 +0300

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -204,8 +204,8 @@ async def auto_filter(bot, update):
             await bot.send_message(
                 chat_id = update.chat.id,
                 text=f"<b>Found</b> {(len_results)} <b>Results For Your Query:</b> <code>{query}</code>"
-                reply_markup=reply_markup,
-                parse_mode="html".
+                reply_markup=reply_markup
+                parse_mode="html"
                 reply_to_message_id=update.message_id
             )
                 
