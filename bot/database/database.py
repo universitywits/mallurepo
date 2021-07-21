f2c6d8cb692157a08a1bab475fc30443c6b6f29d
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2021-07-21 10:55:29 +0530

    Update database.py

--- bot/database/database.py
+++ bot/database/database.py
@@ -493,3 +493,5 @@ class Database(metaclass=Singleton):
         A Funtion to count total filters of a group
         """
         return await self.fcol.count_documents({"group_id": group_id})
+
+    
